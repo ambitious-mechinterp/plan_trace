0c7c468ad2b9abd0dbd62f9ed44cadda236c8630
abhishek9909 <mishra.abhishek199917@gmail.com>
2026-01-17 17:27:10 +0000

    new changes

---
+++ script/sec3_exp_scale_wrapper.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BATCH_SIZE=1
+
+# Prompts from 1 to 10
+PROMPTS=($(seq 1 10))
+
+TOTAL=${#PROMPTS[@]}
+
+i=0
+while [ $i -lt $TOTAL ]; do
+    # Take next batch
+    BATCH=("${PROMPTS[@]:$i:$BATCH_SIZE}")
+
+    BATCH_START=${BATCH[0]}
+    BATCH_END=${BATCH[${#BATCH[@]}-1]}
+    JOB_NAME="plan_scale_test_${BATCH_START}_${BATCH_END}_instruct"
+
+    echo "Submitting batch: ${BATCH[*]}"
+    echo "Job name: $JOB_NAME"
+
+    sbatch --job-name="$JOB_NAME" script/sec3_exp_scale.sh "${BATCH[@]}"
+
+    ((i+=BATCH_SIZE))
+done
